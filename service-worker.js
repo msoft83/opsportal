ccac208e10f6672cbbe03d6b78052f885a92891a
msoft83 <msoft83@gmail.com>
2025-09-05 12:04:30 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "opsportal-v1";
+const urlsToCache = [
+  "index.html",
+  "StationDisplay.html",
+  "StationSetup.html",
+  "AssignedStations.html",
+  "DataManager.html",
+  "TrainingPath.html",
+  "AddLS.html",
+  "SeedData.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
