5f891f396a0d9a40d537ec500d050e1fe658b293
msoft83 <msoft83@gmail.com>
2025-09-08 11:28:58 -0400

    Add files via upload

--- _shared/remote-config.js
+++ _shared/remote-config.js
@@ -1,9 +1,1 @@
-window.__ES_REMOTE_CFG__ = {
-  owner: "msoft83",
-  repo: "opsportal",
-  branch: "main",
-  basePath: "data",
-  token: "",
-  forceHijack: true,
-  verbose: true
-};
+window.__ES_REMOTE_CFG__={owner:"msoft83",repo:"opsportal",branch:"main",basePath:"data",token:"",forceHijack:true,verbose:true};
